2665a5f47529a83cea94b4da45abfad0151d9c19
Saman Sayad <samansayad93@gmail.com>
2023-05-25 17:14:06 +0330

    655

--- 655 quera/1.cpp
+++ 655 quera/1.cpp
@@ -2,18 +2,44 @@
 
 using namespace std;
 
-int main(){
-    int n,j=0;
-    cin>>n;
-    char arr[100][1000],a;
-    for(int i=0;i<n;i++){
-        cin>>a;
-        while(a != '\n'){
-            arr[i][j] = a;
-            cin>>a;
-            j++;
+char check(char a,int i){
+    if(i == 0){
+        if ((int)a>=97&&(int)a<=122){
+            a -= 32;
         }
-        arr[i][j] = 0;
-        j=0;
+        return a;
+    }
+    if(i == 1){
+        if ((int)a>=65&&(int)a<=90){
+            a += 32;
+        }
+        return a;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    char arr[100];
+    for(int i=0;i<n;i++){
+        bool flag = false;
+        cin.getline(arr,100);
+        for(int j=0;j<100;j++){
+            if (flag == false)
+            {
+                flag = true;
+                arr[j] = check(arr[j],0);
+            }
+            if(flag = true){
+                arr[j] = check(arr[j],1);
+            }
+            if(arr[j] == ' '){
+                flag = false;
+            }
+            if(arr[j] == NULL){
+                break;
+            }
+        }
+        cout<<arr<<endl;
     }
 }
